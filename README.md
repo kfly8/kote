9a89aa16ab7d1875e9f283e94708091775261caa
kobaken <kentafly88@gmail.com>
2024-01-21 09:19:48 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+caseval - It's new $module
+
+# SYNOPSIS
+
+    use caseval;
+
+# DESCRIPTION
+
+caseval is ...
+
+# LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kobaken <kentafly88@gmail.com>
---
+++ lib/caseval.pm
@@ -0,0 +1,39 @@
+package caseval;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+caseval - It's new $module
+
+=head1 SYNOPSIS
+
+    use caseval;
+
+=head1 DESCRIPTION
+
+caseval is ...
+
+=head1 LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kobaken E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
