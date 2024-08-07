1c63b8cd498222c057ebd67f8ccc9f8c369f7ebc
kobaken <kentafly88@gmail.com>
2024-08-08 00:04:28 +0900

    add as method

--- lib/Type/Kote.pm
+++ lib/Type/Kote.pm
@@ -26,4 +26,42 @@ sub strictly_create {
 sub create;
 *create = STRICT ? \&strictly_create : sub { ($_[1], undef) };
 
+sub as {
+    my ($self, $type, @args) = @_;
+
+    my $new = $type->parameterize($self, @args);
+    $self->to_kote($new);
+}
+
+# override
+sub child_type_class {
+    __PACKAGE__
+}
+
+# override
+sub _build_complementary_type {
+    my $self = shift;
+    my $type = $self->SUPER::_build_complementary_type();
+    $self->to_kote($type);
+}
+
+sub to_kote {
+    my ($self, $type) = @_;
+
+    if (Scalar::Util::blessed($type) && $type->isa('Type::Kote')) {
+        return $type;
+    }
+
+    $type = Types::TypeTiny::to_TypeTiny($type);
+    if (Scalar::Util::blessed($type) && $type->isa('Type::Tiny')) {
+        return $self->child_type_class->new(
+            display_name => $type->display_name,
+            parent       => $type,
+            library      => $self->library,
+        );
+    }
+
+    $type;
+}
+
 1;
